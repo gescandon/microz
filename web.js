9d583f84f5282a535e43fdd6d4b2754ab1ffb00a
gescandon <gescandon@appirio.com>
2014-10-06 21:20:13 -0600

    add express

--- web.js
+++ web.js
@@ -3,11 +3,13 @@ var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 
+app.set('views', './views')
+app.set('view engine', 'jade')
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/microz.html')
+})
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
